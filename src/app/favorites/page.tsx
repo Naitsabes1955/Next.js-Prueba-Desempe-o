689fd71cbb4070f2f72305a0601fb5cd65cbd6bd
Naitsabes1955 <juansebasmosquera519@gmail.com>
2026-06-23 08:54:29 -0500

    "feat: add future implementation files "

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,10 @@
+export default function FavoritesPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-dark text-white">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Favoritos</h1>
+        <p className="mt-4 text-sm text-muted">Prueba</p>
+      </div>
+    </main>
+  );
+}
